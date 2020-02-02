e4da8629b5e1030664236107cd98209c04ca0a37
Laura S.N <laura.sannicolas@tajamar365.com>
2020-02-02 12:09:04 +0100

    first upload to github

---
+++ EjemploRouting/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace EjemploRouting
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapMvcAttributeRoutes();
+            routes.MapRoute(
+                name: "Custom",
+                url: "{controller}/{action}/{id}/{raza}",
+                defaults: new { controller = "Home", action = "Animal", id = UrlParameter.Optional, raza = UrlParameter.Optional },
+                constraints: new { controller = "Home", action = "Animal", id = "Perro", raza = "^B.*" }
+            );
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ EjemploRouting/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EjemploRouting.Controllers
+{
+    [RoutePrefix("Home")]
+    public class HomeController : Controller
+    {
+        [Route]
+        public ActionResult Index()
+        {      
+            return View();
+        }
+        [Route]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        [Route]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [Route("Animal/{id?}/{raza:regex(^B.*)?}")]
+        public ActionResult Animal(String id, String raza)
+        {
+            ViewBag.Tipo = id;
+            ViewBag.Raza = raza;
+            return View();
+        }
+    }
+}
